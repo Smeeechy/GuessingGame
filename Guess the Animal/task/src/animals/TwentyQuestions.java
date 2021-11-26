6738785d7bc237c77c444efc099f4962fac5cdfe
darynsmith <d.maccoy.smith@gmail.com>
2021-11-25 20:17:11 -0700

    implemented methods for learning (adding new nodes) and evaluating (interactive traversal of the tree), also generified some processes

--- Guess the Animal/task/src/animals/TwentyQuestions.java
+++ Guess the Animal/task/src/animals/TwentyQuestions.java
@@ -6,7 +6,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class TwentyQuestions {
-    private final Scanner scanner = new Scanner(System.in);
+    private final Scanner SCANNER = new Scanner(System.in);
+    private Node root;
 
     private final List<String> YES_WORDS = List.of("Y", "YES", "YEAH", "YEP", "SURE", "RIGHT", "AFFIRMATIVE",
             "CORRECT", "INDEED", "YOU BET", "EXACTLY", "YOU SAID IT");
@@ -14,31 +15,99 @@ public class TwentyQuestions {
             "I DON'T THINK SO", "YEAH NO");
 
     public TwentyQuestions() {
-        init();
+        greet();
+        startup();
+        mainLoop();
+        farewell();
     }
 
-    private void init() {
-        greet();
+    private void startup() {
+        System.out.println("I want to learn about animals. \nWhich animal do you like most?");
+        root = getAnimalFromUser();
+        System.out.println("Wonderful! I've learned so much about animals!\n" +
+                "Let's play a game!");
+    }
 
-        System.out.println("Enter the first animal:");
-        Animal firstAnimal = getAnimalFromUser();
-        System.out.println("Enter the second animal:");
-        Animal secondAnimal = getAnimalFromUser();
+    private void mainLoop() {
+        boolean running = true;
+        while (running) {
+            explainRulesAndConfirm();
+            evaluateFromNode(root);
+            System.out.println("Do you want to play again?");
+            running = validateFromUser();
+        }
+    }
+
+    private void explainRulesAndConfirm() {
+        System.out.println("You think of an animal, and I guess it.\n" +
+                "Press enter when you're ready.");
+        SCANNER.nextLine();
+    }
+
+    private void evaluateFromNode(Node node) {
+        if (node instanceof Animal) {
+            if (!guess((Animal) node)) {
+                // guess was wrong, need to add a fact in place of this animal and the correct one
+                learn(node);
+                System.out.println("Nice! I've learned so much about animals!");
+            } else {
+                System.out.println("I win");
+            }
+        } else if (node instanceof Fact) {
+            System.out.println(((Fact) node).asQuestion());
+            if (validateFromUser()) {
+                evaluateFromNode(node.getRight());
+            } else {
+                evaluateFromNode(node.getLeft());
+            }
+        }
+    }
+
+    private void learn(Node node) {
+        Node parent = node.getParent();
+        System.out.println("I give up. What animal do you have in mind?");
+        Animal actual = getAnimalFromUser();
+        Fact fact = learnDifference((Animal) node, actual);
+        if (root == node) {
+            root = fact;
+        } else {
+            fact.setParent(parent);
+            if (parent.getRight() == node) {
+                parent.setRight(fact);
+            } else {
+                parent.setLeft(fact);
+            }
+        }
+    }
+
+    private boolean guess(Animal animal) {
+        System.out.println("Is it " + animal.toStringIndefinite() + "?");
+        return validateFromUser();
+    }
+
+    private Fact learnDifference(Animal firstAnimal, Animal secondAnimal) {
         Fact fact = getFactFromUser(firstAnimal, secondAnimal);
         System.out.println("Is it correct for " + secondAnimal.toStringIndefinite() + "?");
         boolean trueForSecondAnimal = validateFromUser();
         System.out.println("I learned the following facts about animals:");
         if (trueForSecondAnimal) {
+            fact.setLeft(firstAnimal);
+            firstAnimal.setParent(fact);
+            fact.setRight(secondAnimal);
+            secondAnimal.setParent(fact);
             System.out.println("T" + firstAnimal.toStringDefinite().substring(1) + " " + fact.negate() + ".");
             System.out.println("T" + secondAnimal.toStringDefinite().substring(1) + " " + fact + ".");
         } else {
+            fact.setLeft(secondAnimal);
+            secondAnimal.setParent(fact);
+            fact.setRight(firstAnimal);
+            firstAnimal.setParent(fact);
             System.out.println("T" + firstAnimal.toStringDefinite().substring(1) + " " + fact + ".");
             System.out.println("T" + secondAnimal.toStringDefinite().substring(1) + " " + fact.negate() + ".");
         }
         System.out.println("I can distinguish these animals by asking the question:");
         System.out.println(fact.asQuestion());
-
-        farewell();
+        return fact;
     }
 
     private void askForFact(Animal firstAnimal, Animal secondAnimal) {
@@ -54,7 +123,7 @@ public class TwentyQuestions {
         boolean valid = false;
         do {
             askForFact(firstAnimal, secondAnimal);
-            input = scanner.nextLine().trim();
+            input = SCANNER.nextLine().trim();
             if (input.toLowerCase().matches("^(it) (can|has|is) .*")) {
                 valid = true;
             } else {
@@ -78,7 +147,7 @@ public class TwentyQuestions {
     private boolean validateFromUser() {
         boolean correct;
         while (true) {
-            String response = scanner.nextLine().trim();
+            String response = SCANNER.nextLine().trim();
             if (response.endsWith(".") || response.endsWith("!")) {
                 response = response.substring(0, response.length() - 1);
             }
@@ -99,10 +168,6 @@ public class TwentyQuestions {
         return correct;
     }
 
-    private void makeGuess(String[] articleAndNoun) {
-        System.out.println("Is it " + articleAndNoun[0] + " " + articleAndNoun[1] + "?");
-    }
-
     private void greet() {
         int hour = LocalTime.now().getHour();
         if (hour < 5 || hour >= 18) {
@@ -116,7 +181,7 @@ public class TwentyQuestions {
 
     // getting input from user
     private Animal getAnimalFromUser() {
-        String input = scanner.nextLine().trim().toLowerCase();
+        String input = SCANNER.nextLine().trim().toLowerCase();
         String article, noun;
         if (input.matches("^(a|an)\\s.*")) {
             article = input.split("\\s+")[0];
