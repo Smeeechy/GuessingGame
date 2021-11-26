f21ca70d02fdfbc82f9bff678161737eb3a329d3
darynsmith <d.maccoy.smith@gmail.com>
2021-11-25 20:12:19 -0700

    made Animal class more concise

--- Guess the Animal/task/src/animals/Animal.java
+++ Guess the Animal/task/src/animals/Animal.java
@@ -1,19 +1,12 @@
 package animals;
 
-import java.util.ArrayList;
-
-public class Animal {
+public class Animal extends Node {
     private final String ARTICLE;
     private final String NAME;
-    private ArrayList<Fact> facts;
 
     public Animal(String article, String name) {
         this.ARTICLE = article;
         this.NAME = name;
-    }
-
-    public void addFact(Fact fact) {
-        this.facts.add(fact);
     }
 
     public String toStringDefinite() {
