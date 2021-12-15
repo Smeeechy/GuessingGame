94ad8906f91bccba46407d952e6a7e553f2bdf63
darynsmith <d.maccoy.smith@gmail.com>
2021-12-14 20:35:37 -0700

    wrapped original game loop in another loop which allows selection of several different options including playing the game, listing off animals or facts, and printing the tree

--- Guess the Animal/task/src/animals/Main.java
+++ Guess the Animal/task/src/animals/Main.java
@@ -3,25 +3,69 @@ package animals;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Node root = loadTree(args);
-        TwentyQuestions game;
+        GuessingGame game;
         if (root == null) {
-            game = new TwentyQuestions(null);
+            game = new GuessingGame(null);
             game.setNodeIdGenerator(new NodeIdGenerator(1));
         } else {
-            game = new TwentyQuestions(root);
-            game.setNodeIdGenerator(new NodeIdGenerator(game.getNodeCount()));
+            game = new GuessingGame(root);
+            HashMap<String, Integer> nodeCounts = game.getNodeCount();
+            int nodeTotal = nodeCounts.getOrDefault("animals", 0) + nodeCounts.getOrDefault("facts", 0);
+            game.setNodeIdGenerator(new NodeIdGenerator(nodeTotal));
         }
-        game.start();
+        game.greet();
+        game.startup();
         saveTree(args, game.getRoot());
+        System.out.println("Welcome to the animal expert system!\n");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            displayMenu();
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    game.playGuessingGame();
+                    saveTree(args, game.getRoot());
+                    break;
+                case 2:
+                    game.printListOfAnimals();
+                    break;
+                case 3:
+                    game.printFactsAboutAnimal();
+                    break;
+                case 4:
+                    game.showStats();
+                    break;
+                case 5:
+                    game.printTree();
+                    break;
+                case 0:
+                default:
+                    running = false;
+                    break;
+            }
+        }
+        game.farewell();
+    }
+
+    private static void displayMenu() {
+        System.out.println("What do you want to do:\n" +
+                "\n" +
+                "1. Play the guessing game\n" +
+                "2. List of all animals\n" +
+                "3. Search for an animal\n" +
+                "4. Calculate statistics\n" +
+                "5. Print the Knowledge Tree\n" +
+                "0. Exit");
     }
 
     private static void saveTree(String[] args, Node root) {
@@ -48,7 +92,6 @@ public class Main {
         try {
             return mapper.readValue(file, Node.class);
         } catch (IOException ioException) {
-            System.out.println(ioException.getMessage());
             return null;
         }
     }
@@ -60,7 +103,7 @@ public class Main {
                 case "xml":
                     mapper = new XmlMapper();
                     break;
-             /* maybe jackson's yamlmapper is busted? it breaks everything.
+             /* maybe jackson's yaml mapper is busted? it breaks everything.
                 serializes just fine, but refuses to deserialize properly */
 //                case "yaml":
 //                    mapper = new YAMLMapper();
